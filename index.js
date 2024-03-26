3e67698986091ef26fafbfb3ae52c72f17b0cb42
Nagesh213 <164941528+Nagesh213@users.noreply.github.com>
2024-03-26 23:24:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const startingminutes = window.prompt("enter the timer");
+let time = startingminutes * 60;
+
+const countdownele = document.getElementById("countdown");
+
+setInterval(updatecountdown , 1000);
+
+function updatecountdown() {
+const minutes = Math.floor(time/60);
+let seconds = time % 60;
+
+seconds = seconds < 10 ? '0' + seconds : seconds;
+
+
+countdownele.innerHTML = `${minutes} : ${seconds}`;
+time--;
+
+}
